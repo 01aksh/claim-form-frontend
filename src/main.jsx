933e77a27f09a756bddbef09efc80979d6c04b1f
01aksh <akshaylokapur@gmail.com>
2025-04-06 22:05:36 +0530

    Context API Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { CounterProvider } from "./context/Example.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CounterProvider>
+      <App />
+    </CounterProvider>
+  </React.StrictMode>
+);
+
+
+// import { useContext } from "react";
+// import { CounterContext } from "./context/Example";
+
+// const App = () => {
+//   const { count, setCount } = useContext(CounterContext);
+//   return (
+//     <div>
+//       <h1>Welcome to the React App</h1>
+//       <div className="text-center">
+//         <h3 className="text-2xl">Count is: {count}</h3>
+//         <button
+//           onClick={() => setCount(count + 1)}
+//           className="p-2 text-white bg-pink-600 rounded"
+//         >
+//           Increase Count
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  jsx: 'react', // Ensure JSX is supported for .js files
+
 })
